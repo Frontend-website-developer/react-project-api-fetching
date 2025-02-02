538202edcbdd779443640e92f13b321761d45b55
Frontend-website-developer <ssajidali.dev@gmail.com>
2025-02-02 09:43:26 -0800

    project main files

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Search({searchTerm, setsearchTerm}) {
+    return (
+        <div className='search'>
+            <div>
+                <img src='././search.png' alt='search-icon' />
+                <input type="text" placeholder='Search through thousands of movies' value={searchTerm} onChange={(e) => {
+                    setsearchTerm(e.target.value) 
+                }} />
+            </div>
+        </div>
+    )
+}
+
+export default Search
